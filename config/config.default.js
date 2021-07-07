10713e7b3ebfd0b671fa3407943b69f28fd12d87
luojunbang <luojunbang@foxmail.com>
2021-07-07 10:16:30 +0800

    config

--- config/config.default.js
+++ config/config.default.js
@@ -1,9 +1,23 @@
-'use strict'
-const { APP_ID, APP_SECRET } = require('../app.config.js')
-exports.keys = 'lo'
+"use strict";
 
-const WECHAT_CONFIG = { APP_ID, APP_SECRET }
+// const fs = require("fs/promises");
+// const { join } = require("path");
+let APP_CONFIG = {}
+try {
+  // fs.statSync(join(__dirname, "../app.config.js"));
+  APP_CONFIG = require("../app.config.js");
+} catch (e) {
+  console.log('........','ATTENTION!! CANT FIND FILE app.config.js FOR APP','........');
+  APP_CONFIG = {
+    APP_ID: "APP_ID",
+    APP_SECRET: "APP_SECRET",
+  };
+}
+
+exports.keys = "lo";
+const { APP_ID, APP_SECRET } = APP_CONFIG;
 
 exports.wechat = {
-  AUTH_URL: (code = '') => `https://api.weixin.qq.com/sns/jscode2session?appid=${APP_ID}&secret=${APP_SECRET}&js_code=${code}&grant_type=authorization_code`,
-}
+  AUTH_URL: (code = "") =>
+    `https://api.weixin.qq.com/sns/jscode2session?appid=${APP_ID}&secret=${APP_SECRET}&js_code=${code}&grant_type=authorization_code`,
+};
